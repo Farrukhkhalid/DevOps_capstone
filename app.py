677096083642e771dfc27c01d01a5b36348a612e
Farrukhkhalid <farrukhkhalid.it@outlook.com>
2022-03-03 01:04:29 +0500

    final

--- app.py
+++ app.py
@@ -2,19 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Congratulations, it's a web app!"
 
 @app.route("/")
-def fahrenheit_from(celsius):
-    """Convert Celsius to Fahrenheit degrees."""
-    try:
-        fahrenheit = float(celsius) * 9 / 5 + 32
-        fahrenheit = round(fahrenheit, 3)  # Round to three decimal places
-        return str(fahrenheit)
-    except ValueError:
-        return "invalid input"
+def home():
+    html = f"<h1>Hi again!</h1><p>Welcome TO Cloud DevOps Engineer Nanodegree Program  Capstone project </p>"
+    return html.format(format)
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)  # specify port=80
